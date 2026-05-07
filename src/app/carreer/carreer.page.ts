97c6c3ffed48d66bbfde7d6ea317c930cbd72090
Ian Grant <globosian2018@gmail.com>
2026-05-07 13:33:58 -0600

    Trabajo de Granta

--- src/app/carreer/carreer.page.ts
+++ src/app/carreer/carreer.page.ts
@@ -1,18 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { IonButton, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-carreer',
   templateUrl: './carreer.page.html',
   styleUrls: ['./carreer.page.scss'],
   standalone: true,
-  imports: [IonContent],
+  imports: [IonContent, IonButton, RouterLink],
 })
-export class CarreerPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class CarreerPage {}
